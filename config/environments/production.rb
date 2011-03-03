ce4b6e895000f0bca6b8beac14ecf118ab79e5c9
Martin Provencher <vivrass@UbuntuProject.(none)>
2011-03-02 22:45:35 -0500

    Add the home controller Generate files for Devise (login) Add title helper

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ RailsTemplate::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'rails_template.com' }
 end
