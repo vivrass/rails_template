4408010fdb237f6aa87d1c647060415681bae8e1
Martin Provencher <vivrass@UbuntuProject.(none)>
2011-03-02 19:36:23 -0500

    Create rails without prototype and test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTemplate::Application.config.secret_token = '1b3b1a8b5cb67051978f358d678638d7af7b10d356086b6204f5272f70d579a4368158846d0dffabc01de89ed0e2372c1e20ceea79b7f30efeea717ec690772a'
